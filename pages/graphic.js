095b154b75c0b0409a8c5c7e2192e1b7917b5d96
wndererk <bangwan.ap@hotmail.com>
2021-07-06 16:12:10 +0700

    Final GraphicPage

--- pages/graphic.js
+++ pages/graphic.js
@@ -1,21 +1,33 @@
-import React from 'react'
+import React ,{ useState , useEffect } from 'react'
 import Image from 'next/image'
 import jojiwalk from '../public/image/jojiwalk.gif'
 import jojijump from '../public/image/jojijump.gif'
 import cat from '../public/image/cat.png'
-import town from '../public/image/town_example.png'
 import styles from '../styles/Home.module.css';
+import { Button } from 'antd';
 
 export default function graphic() {
+    const [isWalk,setIswalk] = useState(false);
+    const [isJump,setIsjump] = useState(true);
+
+    const onCLick = () => {
+        setIswalk(!isWalk); 
+        setIsjump(!isJump);
+    }
+
+
     
     return (
         <div className={`${styles.graphic} ${styles.container}`}>
             <h1 className={styles.title}>Graphic Design</h1>
             <div className={styles.flexbox}>
-                <div className={styles.walk}><Image src={jojiwalk} alt="jojiwalk"/></div>
-                <div className={styles.jump}><Image src={jojijump} alt="jojijump"/></div>
+                <div className={styles.walk} style={{display: isWalk?'block':'none'}}><Image src={jojiwalk} alt="jojiwalk"/></div>
+                <div className={styles.jump} style={{display: isJump?'block':'none'}} ><Image src={jojijump} alt="jojijump"/></div>
                 <div className={styles.cat}><Image src={cat} alt="cat"/></div>
             </div>
+            <Button onClick={onCLick} disabled={isWalk}>Walk</Button> &nbsp;
+            <Button onClick={onCLick} disabled={isJump}>Jump</Button>
+            
             {/* <Image src={town} alt="town" styles={{width : 200, height: 200}}/> */}
         </div>
     )
