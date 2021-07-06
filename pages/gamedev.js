a09a98a992e449d7daf4cf4f2f4850a84516086a
wndererk <bangwan.ap@hotmail.com>
2021-07-06 18:00:26 +0700

    Styling WebPage

--- pages/gamedev.js
+++ pages/gamedev.js
@@ -6,14 +6,14 @@ export default function Gamedev() {
     return (
         <div className={`${styles.gamedev} ${styles.container}`}>
             <h1 className={styles.title}>Game Development</h1>
-            <div className={styles.boxitem}>
-                <div className={styles.item}>
+            <div className={styles.gamebox}>
+                <div className={`${styles.item} ${styles.gameitem}`}>
                     <ReactPlayer url='https://firebasestorage.googleapis.com/v0/b/portfolio-f502a.appspot.com/o/MiniGolf.mp4?alt=media&token=b351c78f-8ab8-477e-9a97-2bc40643e2f1' controls />
                 </div>
-                <div className={styles.item}>
+                <div className={`${styles.item} ${styles.gameitem}`}>
                     <ReactPlayer url='https://firebasestorage.googleapis.com/v0/b/portfolio-f502a.appspot.com/o/GoJoji.mp4?alt=media&token=7677db00-d769-4d9b-9880-a9cc245e9f6e' controls />
                 </div>
-                <div className={styles.item}>
+                <div className={`${styles.item} ${styles.gameitem}`}>
                     <ReactPlayer url='https://firebasestorage.googleapis.com/v0/b/portfolio-f502a.appspot.com/o/Platformer.mp4?alt=media&token=4313d56a-ba16-4f1d-9acd-974582328698' controls />
                 </div>                
             </div>
--- pages/webdev.js
+++ pages/webdev.js
@@ -6,10 +6,10 @@ export default function Webdev() {
     return (
         <div className={`${styles.webdev} ${styles.container}`}>
             <h1 className={styles.title}>Web Development</h1>
-            <div className={styles.item}>
-                <ReactPlayer url="https://firebasestorage.googleapis.com/v0/b/portfolio-f502a.appspot.com/o/Dudushop.mp4?alt=media&token=c27598e1-09d7-4da3-8f23-b97f36515706" controls/>
-                <ReactPlayer url="https://firebasestorage.googleapis.com/v0/b/portfolio-f502a.appspot.com/o/register.mp4?alt=media&token=90367713-a04c-430c-aca1-bbc7c1d723b0" controls/>
-                <ReactPlayer url="https://firebasestorage.googleapis.com/v0/b/portfolio-f502a.appspot.com/o/postbook.mp4?alt=media&token=05ff820d-a070-42b5-bb8a-4bf90a34a645" controls/>
+            <div className={styles.webbox}>
+                <div className={`${styles.item} ${styles.webitem}`}><ReactPlayer url="https://firebasestorage.googleapis.com/v0/b/portfolio-f502a.appspot.com/o/Dudushop.mp4?alt=media&token=c27598e1-09d7-4da3-8f23-b97f36515706" controls/></div>
+                <div className={`${styles.item} ${styles.webitem}`}><ReactPlayer url="https://firebasestorage.googleapis.com/v0/b/portfolio-f502a.appspot.com/o/register.mp4?alt=media&token=90367713-a04c-430c-aca1-bbc7c1d723b0" controls/></div>
+                <div className={`${styles.item} ${styles.webitem}`}><ReactPlayer url="https://firebasestorage.googleapis.com/v0/b/portfolio-f502a.appspot.com/o/postbook.mp4?alt=media&token=05ff820d-a070-42b5-bb8a-4bf90a34a645" controls/></div>
             </div>         
         </div>
     )
