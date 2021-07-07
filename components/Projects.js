4211b963157e041a8039df7b12c1bce54e47b814
wndererk <bangwan.ap@hotmail.com>
2021-07-07 16:20:21 +0700

    Edit Responsive

--- components/Projects.js
+++ components/Projects.js
@@ -13,40 +13,42 @@ function Projects(){
     return(
         <div className={styles.projects}>
         <h1 id="Projects">All Projects</h1>
-        <div className="site-card-wrapper" style={{ marginLeft: 30, marginTop: 30 }}>
-            <Row gutter={16}>
-                <Col span={8}>
-                    <Link href="/webdev" passHref>
-                        <Badge style={{ backgroundColor: '#108ee9' }} count={3}>
-                        <Card hoverable style={{ width: 400 }}
-                            cover={<Image alt="Web Development" src={web}/>}>
-                            <Meta title="Web Development"/>
-                        </Card>
-                        </Badge>
-                    </Link>
-                </Col>
-                <Col span={8}>
-                    <Link href="/gamedev" passHref>
-                        <Badge style={{ backgroundColor: '#108ee9' }} count={3}>
-                        <Card hoverable style={{ width: 400 }}
-                            cover={<Image alt="Game Development" src={game}/>}>
-                            <Meta title="Game Development"/>
-                        </Card>
-                        </Badge>
-                    </Link>
-                </Col>
-                <Col span={8}>
-                    <Link href="/graphic" passHref>
-                        <Badge style={{ backgroundColor: '#108ee9' }} count={1}>
-                        <Card hoverable style={{ width: 400 }}
-                            cover={<Image alt="Graphic Design" src={graphic}/>}>
-                            <Meta title="Graphic Design"/>
-                        </Card>
-                        </Badge>
-                    </Link>
-                </Col>
-            </Row>
-        </div>
+        <Row>
+            <div className="site-card-wrapper" style={{ marginLeft: 30, marginTop: 30 }}>
+                <Row gutter={16}>
+                    <Col span={8}>
+                        <Link href="/webdev" passHref>
+                            <Badge style={{ backgroundColor: '#108ee9' }} count={3}>
+                                <Card hoverable
+                                    cover={<Image alt="Web Development" src={web}/>}>
+                                    <Meta title="Web Development"/>
+                                </Card>
+                            </Badge>
+                        </Link>
+                    </Col>
+                    <Col span={8}>
+                        <Link href="/gamedev" passHref>
+                            <Badge style={{ backgroundColor: '#108ee9' }} count={3}>
+                                <Card hoverable
+                                    cover={<Image alt="Game Development" src={game}/>}>
+                                    <Meta title="Game Development"/>
+                                </Card>
+                            </Badge>
+                        </Link>
+                    </Col>
+                    <Col span={8}>
+                        <Link href="/graphic" passHref>
+                            <Badge style={{ backgroundColor: '#108ee9' }} count={1}>
+                                <Card hoverable
+                                    cover={<Image alt="Graphic Design" src={graphic}/>}>
+                                    <Meta title="Graphic Design"/>
+                                </Card>
+                            </Badge>
+                        </Link>
+                    </Col>
+                </Row>
+            </div>
+        </Row>
         </div>
     )
 }
